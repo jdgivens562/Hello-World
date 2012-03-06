b4294e70df610cc4badac8c5b40b7bb4b2e390bb
jdgivens562 <julian.dan.givens@hp.com>
2012-03-06 16:28:02 -0600

    adding my hello world prog to repo

---
+++ HelloWorld.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+char fname[50];
+char lname[50];
+
+cout << "Please enter your first name:" << endl;
+cin >> fname;
+
+cout << "Please enter your last name:" << endl;
+cin >> lname;
+
+cout << "Hello, " << fname << lname << "Welcome to the World" << endl;
+
+return 0;
+
+
+
+
+}
